7e1c545e2b4a383eae980c9c2fdb44a529487309
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-11-12 09:28:22 +0100

    Fortsatte med NumberDisplayen med values

--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -2,9 +2,15 @@ public class NumberDisplay {
     private int limit;
     private int value;
 
-    public NumberDisplay() {
-        getValue =
-
+    public NumberDisplay(int maxLimit) {
+        value = 0;
+        limit = maxLimit;
+    }
+    public int getValue(){
+       return value;
+    }
+    public void setValue(int newValue){
+        if (newValue < 0
 
     }
 }
